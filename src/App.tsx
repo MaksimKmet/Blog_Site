e72ef3db2881abfea7c33f0525733b75c9cc1d76
MaksimKmet <you@example.com>
2024-11-18 19:02:27 +0100

    rebuild from ... to react ts tailwind vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <nav></nav>
+    <div className="flex justify-center">
+    <p className='px-2 text-2xl text-center text-green-600'>Yes</p>
+    <p className='px-2 text-2xl text-center text-green-600'>No </p>
+    </div>
     </>
   )
 }
