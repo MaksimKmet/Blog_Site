46f67cace012b5fb72b53706070d8986880c01eb
MaksimKmet <you@example.com>
2024-11-18 20:33:22 +0100

    create route and style hover to links,

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
+// import {Home, Services, Menu, Contact} from './components/pages'
+import Home from './components/pages/Home'
+import Menu from './components/pages/Menu'
+import Services from './components/pages/Services'
+import Contact from './components/pages/Contact'
+
 
 function App() {
 
   return (
     <>
-    <nav></nav>
-    <div className="flex justify-center">
-    <p className='px-2 text-2xl text-center text-green-600'>Yes</p>
-    <p className='px-2 text-2xl text-center text-green-600'>No </p>
-    </div>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/Menu' element={<Menu />}></Route>
+      <Route path='/Services' element={<Services />}></Route>
+      <Route path='/Contact' element={<Contact />}></Route>
+    </Routes>
     </>
   )
 }
