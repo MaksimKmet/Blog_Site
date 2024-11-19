80a9afa17a892ce22bf1c67713cccca3a86d68b7
MaksimKmet <you@example.com>
2024-11-19 21:48:38 +0100

    create menu file, not finish yet card file fix the bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/Navbar";
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
 // import {Home, Services, Menu, Contact} from './components/pages'
-import Home from "./pages/Home";
-import Menu from "./pages/Menu";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+
 
 function App() {
+
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/Menu" element={<Menu />}></Route>
-        <Route path="/Services" element={<Services />}></Route>
-        <Route path="/Contact" element={<Contact />}></Route>
-      </Routes>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/Menu' element={<Menu />}></Route>
+      <Route path='/Services' element={<Services />}></Route>
+      <Route path='/Contact' element={<Contact />}></Route>
+    </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,15 @@
+import { Menu } from "./pizzaMenu"
+
 const Card = () => {
   return (
     <div className="flex justify-center p-4">
-      <img
-        src="https://img.freepik.com/free-photo/slice-crispy-pizza-with-meat-cheese_140725-6974.jpg"
-        alt=""
-        width={"200px"}
-      />
-    </div>
-  );
-};
+        <div className="">
 
-export default Card;
+      <img src="https://img.freepik.com/free-photo/slice-crispy-pizza-with-meat-cheese_140725-6974.jpg" alt="" width={"200px"} />
+      <h3 onClick={()=> console.log(Menu)} className="text-center"> Pizza z serem</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Card
