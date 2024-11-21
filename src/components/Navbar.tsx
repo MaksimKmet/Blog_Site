3d7498a20fa49834208456e5065462a63070ad00
MaksimKmet <you@example.com>
2024-11-21 08:01:01 +0100

    task with routes error done, initial

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,17 +5,17 @@ const Navbar = () => {
     <>
       <nav className="flex justify-between h-min py-2 bg-gray-100">
         <h1 className="px-2 text-2xl text-blue-900 hover:text-red-500">
-          <Link to={"/"}>Home</Link>
+          <Link to={"../"}>Home</Link>
         </h1>
         <ul className="flex items-center ">
           <li className="px-2 hover:text-red-500">
-            <NavLink to="/Menu">Menu</NavLink>
+            <NavLink to="../Menu">Menu</NavLink>
           </li>
           <li className="px-2 hover:text-red-500">
-            <NavLink to="/Services">Services</NavLink>
+            <NavLink to="../Services">Services</NavLink>
           </li>
           <li className="px-2 hover:text-red-500">
-            <NavLink to="/Contact">Contact</NavLink>
+            <NavLink to="../Contact">Contact</NavLink>
           </li>
         </ul>
       </nav>
